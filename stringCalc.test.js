d407999c52be2327bb213e6d97b029f254b03085
Arnar Elvarsson <arnar91@gmail.com>
2018-10-11 14:44:12 +0000

    Kata part 2 completed - string solution accepts any string length as long as numbers are seperated by the delimiter

--- stringCalc.test.js
+++ stringCalc.test.js
@@ -11,6 +11,10 @@ test("Empty string returns 0", () => {
 	expect(tester.add("1")).toBe(1);
 	});
 	
-	test("two numbers return the sum", () => {
+	test("two numbers return the sum: 1 + 2 = 3", () => {
 	expect(tester.add("1, 2")).toBe(3);
+	});
+	
+	test("Lets try more than two numbers: 1+2+3+4 = 10", () => {
+	expect(tester.add("1,2,3,4",)).toBe(10);
 	});
